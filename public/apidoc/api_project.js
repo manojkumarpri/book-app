db55e3a7970d1cdac62e04782eddae229735f21b
ManojKumar2020 <manojkumar.naga02@gmail.com>
2020-05-21 01:57:33 +0530

    commit

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,14 +1,14 @@
 define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
+  "name": "kongumalai API",
+  "version": "1.0.0",
+  "description": "Documentation for kongumalai API",
   "sampleUrl": false,
-  "defaultVersion": "0.0.0",
+  "defaultVersion": "1.0.0",
   "apidoc": "0.3.0",
   "generator": {
-    "name": "apidoc",
+    "name": "kongumalai",
     "time": "2020-05-20T20:21:16.395Z",
-    "url": "http://apidocjs.com",
+    "url": "http://shinelogics.com/",
     "version": "0.22.1"
   }
 });
